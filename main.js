3f506ddb4facccf9531bcf98af52db2a7bbc90d0
ZombieBrine1309 <51412993+ZombieBrine13092@users.noreply.github.com>
2021-12-05 02:20:25 -0500

    Copy test

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 // zombify the input string
 function convertString() {
-
     var inputStringValue = document.getElementById("stringInput").value;
-
     document.getElementById("stringOutput").innerHTML = zombify(inputStringValue);
 }
 
 // Throw errors beside the convert button (unused for the time being)
 function throwError(errorString) {
     document.getElementById("errorText").innerHTML = errorString;
+}
+
+function copyText() {
+    var inputStringValue = document.getElementById("stringInput").value;
+    var output = zombify(inputStringValue)
+    navigator.clipboard.writeText(output);
 }
